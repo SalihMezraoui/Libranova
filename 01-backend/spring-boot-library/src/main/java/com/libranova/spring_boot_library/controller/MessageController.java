94a1177233918285b9c7072d264f3c24759f8816
SalihMezraoui <salih.mezraoui@gmail.com>
2025-07-01 23:56:26 +0200

    Implementing admin services - Messages

--- 01-backend/spring-boot-library/src/main/java/com/libranova/spring_boot_library/controller/MessageController.java
+++ 01-backend/spring-boot-library/src/main/java/com/libranova/spring_boot_library/controller/MessageController.java
@@ -1,10 +1,15 @@
 package com.libranova.spring_boot_library.controller;
 
+import com.libranova.spring_boot_library.dto.request.QuestionRequest;
 import com.libranova.spring_boot_library.model.Message;
 import com.libranova.spring_boot_library.service.MessageService;
+import com.libranova.spring_boot_library.utils.RetrieveJWT;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -19,6 +24,24 @@ public class MessageController
         String userEmail = authentication.getName();
         messageService.saveMessage(messageRequest, userEmail);
     }
+
+    @PutMapping("/secure/admin/message")
+    public void updateMessageResponse(@RequestBody QuestionRequest questionRequest, Authentication authentication) throws AccessDeniedException {
+        String userEmail = authentication.getName();
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuth) {
+            String admin = jwtAuth.getToken().getClaim("userType");
+
+            if (admin == null || !admin.equals("admin")) {
+                throw new AccessDeniedException("Unauthorized access: Only admins can update messages.");
+            }
+
+            messageService.updateMessageResponse(questionRequest, userEmail);
+        } else {
+            throw new AccessDeniedException("Unauthorized: Invalid token type.");
+        }
+    }
+
 
 
 }

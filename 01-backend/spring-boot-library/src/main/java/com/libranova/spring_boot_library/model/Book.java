32c57b407ab32e11a64f5c79e82cc34f332696f5
SalihMezraoui <salih.mezraoui@gmail.com>
2025-07-09 12:57:41 +0200

    Frontend - Add a new book

--- 01-backend/spring-boot-library/src/main/java/com/libranova/spring_boot_library/model/Book.java
+++ 01-backend/spring-boot-library/src/main/java/com/libranova/spring_boot_library/model/Book.java
@@ -1,13 +1,17 @@
 package com.libranova.spring_boot_library.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "book")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book
 {
     @Id
